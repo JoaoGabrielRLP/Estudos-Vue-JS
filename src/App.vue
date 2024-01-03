8b65a82a98025e3f25b40242745a06fcf011523d
JoaoGabrielRLP <joojgaag@gmail.com>
2024-01-03 20:49:53 -0300

    Primeiro commit - realizada a instalação do Vue CLI e avaliando declarações condicionais do vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <TheHeader v-if="showHeader"  />
+
+  <div v-show="showName">
+    Nome: {{ firstName }} <br>
+    Sobrenome: {{ lastName }}
+  </div>
+
+  <div v-if="acessLevel === 'admin'">Admin</div>
+  <div v-else-if="acessLevel === 'marketing'">Marketing</div>
+  <div v-else>User</div>
+  
+  <img 
+    
+    alt="Vue logo" 
+    src="./assets/logo.png"
+  
+  >
+  
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TheHeader
+  },
+
+  data(){
+    return {
+      showHeader: true,
+      firstName: 'Supremo',
+      lastName: 'senhor Kaioh',
+      showName: false,
+      acessLevel: 'marketing'
+    }
   }
 }
 </script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="">
+        Header
+    </header>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    header {
+        background: #000000;
+        color: #fff;
+    }
+</style>
